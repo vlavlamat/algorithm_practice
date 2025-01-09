055e8fe72da39a34db5d171c140cf5b4e4d18cb5
Vladimir Matkovskii <vlavlamat@icloud.com>
2025-01-08 21:50:28 -0400

    Setup 'Digit Multiplication Reduction' task

--- src/bin/digit_product.rs
+++ src/bin/digit_product.rs
@@ -1,3 +1,27 @@
+#![allow(dead_code)]
+/*
+    Write a function that calculates the product of the digits of a number, ignoring zeros. Repeat this operation on the result until a single-digit number is obtained.
+*/
+
+fn digit_product(n: u32) -> u8 {
+    todo!("Product of digits")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(digit_product(0), 0);
+        assert_eq!(digit_product(9), 9);
+        assert_eq!(digit_product(10), 1);
+        assert_eq!(digit_product(987), 2); // 9*8*7=504, 5*4=20, 2
+        assert_eq!(digit_product(123456), 4); // 1*2*3*4*5*6=720, 7*2=14, 1*4=4
+        assert_eq!(digit_product(123454321), 6); // 1*2*3*4*5*4*3*2*1=2880, 2*8*8=128, 1*2*8=16, 1*6=6
+    }
+}
+
 fn main() {
-    println!("{}", 1234567890);
+    digit_product(123456);
 }
